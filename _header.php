f3507bf5db20c6557d0040e962429668b6bea5c1
Alexander O'Hare <alexanderohare@Alexanders-MacBook-Pro.local>
2017-02-26 16:55:12 -0500

    Login js

--- _header.php
+++ _header.php
@@ -3,7 +3,8 @@
   <head>
     <meta charset="utf-8">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-    <link rel="stylesheet" href="css/styles.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+    <link rel="stylesheet" href="css/home.css">
     <script src="js/script.js"></script>
     <title>Help NY</title>
   </head>
