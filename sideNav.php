5e94d46e3e83fa2bfb9f08283333e63433b17d1d
angkimbrell1 <kimb7669@fredonia.edu>
2017-02-26 17:01:44 -0500

    Side navbar

--- sideNav.php
+++ sideNav.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="utf-8">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-    <link rel="stylesheet" href="css/help-ny.css">
+    <link rel="stylesheet" href="css/navbar.css">
     <title></title>
   </head>
   <body>
@@ -11,30 +11,23 @@
   <div class="row">
     <div class="col-sm-3">
       <div class="sidebar-nav">
-        <div class="navbar navbar-default" role="navigation">
-          <div class="navbar-header">
-            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".sidebar-navbar-collapse">
-              <span class="sr-only">Toggle navigation</span>
-              <span class="icon-bar"></span>
-              <span class="icon-bar"></span>
-              <span class="icon-bar"></span>
-            </button>
-            <span class="visible-xs navbar-brand">Sidebar menu</span>
-          </div>
-          <div class="navbar-collapse collapse sidebar-navbar-collapse">
+        <!-- <div class="navbar navbar-default" role="navigation"> -->
+          <p><a href="#main_nav" id="access_nav" class="access_aid">Skip to navigation</a></p><br>
+          <nav id="main_nav">
             <ul class="nav navbar-nav">
-              <li><a href="#">Home</a></li><br>
-              <li><a href="#">College Checklist</a></li><br>
+              <li><a href="#">Overview</a></li><br>
+              <li><a href="#">Resources</a></li><br>
               <li><a href="#">Statistics</a></li><br>
-              <li><a href="#">Login</a></li><br>
+              <li><a href="#">Colleges</a></li><br>
+              <li><a href="#">Majors</a></li>
+              <li><a href="#">Settings</a></li>
             </ul>
-          </div><!--/.nav-collapse -->
-        </div>
+            <p><a href="#body" id="access_top" class="access_aid">Skip to top</a></p>
+          </nav>
+        <!-- </div> -->
       </div>
     </div>
   </div>
   </div>
-
-
   </body>
 </html>
