eb636d193a14e4c7deb85ae80717c0ee94b00174
Alexander James O'Hare <ohar4307@fredonia.edu>
2017-02-27 14:46:12 -0500

    Portal main switch pseudo added

--- portal.php
+++ portal.php
@@ -4,6 +4,36 @@
 		<?php include 'sideNav.php' ?>
 	</div>
 	<div id="portalMainContent" class="col-md-11">
-		<?php //Selector for which page to display ?>
+		<?php
+			$current_page = isset($_GET['page']) ? $_GET['page'] : null;
+			switch ($current_page) {
+				case ('overview'):
+					include 'overview.php';
+					break;
+				case ('checklist'):
+					include 'checklist.php';
+					break;
+				case ('applications'):
+					include 'applications.php';
+					break;
+				case ('colleges'):
+					include 'colleges.php';
+					break;
+				case ('majors'):
+					include 'majors.php';
+					break;
+				case ('statistics'):
+					include 'statistics.php';
+					break;
+				case ('resources'):
+					include 'resources.php';
+					break;
+				case ('settings'):
+					include 'settings.php';
+					break;
+				default:
+					break;
+			}
+		?>
 	</div>
 </body>
